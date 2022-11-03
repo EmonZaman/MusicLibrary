f20304d37a0a6162cef07a45443797b2d8a5b5ef
EmonZaman <saad.twinbit@gmail.com>
2022-11-03 12:24:40 +0600

    UITable view and searchbar added

---
+++ MusicLibrary/CustomTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CustomTableViewCell.swift
+//  MusicLibrary
+//
+//  Created by Twinbit Limited on 3/11/22.
+//
+
+import UIKit
+class CustomeTableViewCell: UITableViewCell{
+    
+    @IBOutlet weak var lblCell: UILabel!
+    @IBOutlet weak var imgImage: UIImageView!
+}
--- MusicLibrary/ViewController.swift
+++ MusicLibrary/ViewController.swift
@@ -8,12 +8,68 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var musicLibrayTableView: UITableView!
+    lazy var searchBar = UISearchBar(frame: .zero)
+    private lazy var searchController: UISearchController = {
+          let sc = UISearchController(searchResultsController: nil)
+          sc.searchResultsUpdater = self
+          sc.delegate = self
+          sc.obscuresBackgroundDuringPresentation = false
+          sc.searchBar.placeholder = "Search"
+          sc.searchBar.autocapitalizationType = .allCharacters
+          return sc
+      }()
+    private func setupNavigationBar() {
+        navigationItem.searchController = searchController
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationItem.title = "Music Library"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        searchBar.placeholder = "Search"
+        setupNavigationBar()
+        musicLibrayTableView.delegate = self
+        musicLibrayTableView.dataSource = self
+       
     }
 
 
 }
 
+extension ViewController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You taped me")
+    }
+    
+}
+
+extension ViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = musicLibrayTableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomeTableViewCell
+        
+        cell.lblCell.text = "Album"
+        
+     
+       return cell
+        
+    }
+     
+}
+extension ViewController: UISearchResultsUpdating, UISearchControllerDelegate {
+    func updateSearchResults(for searchController: UISearchController) {
+        
+        print(searchController.searchBar.text)
+       
+        //        else {
+        //         filteredObjects = sections.filter{ $0secctions.data.name.range(of: searchString, options: .caseInsensitive) != nil }
+        //       }
+ 
+        
+    }
+}
